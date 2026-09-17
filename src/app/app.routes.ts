216331e2ac2707b8a0b8889484ad278841b75976
Riky <ra_mo116@hotmail.com>
2026-09-16 21:20:02 -0700

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './components/home/home';
+import { Contact } from './components/contact/contact';
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'home', component: Home },
+    { path: 'contact', component: Contact}
+];
